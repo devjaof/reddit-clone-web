d38b78951252e7f0c3b7318bc063695ef0808094
João Francisco <joao@Joaos-MacBook-Pro.local>
2023-03-07 21:56:58 -0300

    altera versões, corrige codegen e realiza update da me query quando o user loga ou register

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,18 @@
-import { ChakraProvider } from '@chakra-ui/react'
-
 import theme from '../theme'
+import { ChakraProvider } from '@chakra-ui/react'
 import { AppProps } from 'next/app'
 import { createClient, Provider,dedupExchange, fetchExchange } from 'urql'
+import { Cache, cacheExchange, QueryInput } from '@urql/exchange-graphcache'
+import { LoginMutation, MeDocument, MeQuery, RegisterMutation } from '../generated/graphql'
 
-import { cacheExchange } from '@urql/exchange-graphcache'
+function betterUpdateQuery<Result, Query>(
+  cache: Cache,
+  qi: QueryInput,
+  result: any,
+  fn: (r: Result, q: Query) => Query
+) {
+  return cache.updateQuery(qi, data => fn(result, data as any) as any);
+}
 
 const client = createClient({ 
   url: 'http://localhost:4400/graphql', 
@@ -14,8 +22,36 @@ const client = createClient({
   exchanges: [dedupExchange, cacheExchange({
     updates: {
       Mutation: {
-        login: (result, args, cache, info) => {
-          // cache.updateQuery({ query: MeDocument })
+        login: (_result, args, cache, info) => {
+          betterUpdateQuery<LoginMutation, MeQuery>(cache, 
+            {query: MeDocument},
+            _result,
+            (result, query) => {
+              if (result.login.errors) {
+                return query;
+              } else {
+                return {
+                  me: result.login.user
+                }
+              }
+            }
+          )
+        },
+
+        register: (_result, args, cache, info) => {
+          betterUpdateQuery<RegisterMutation, MeQuery>(cache, 
+            {query: MeDocument},
+            _result,
+            (result, query) => {
+              if (result.register.errors) {
+                return query;
+              } else {
+                return {
+                  me: result.register.user
+                }
+              }
+            }
+          )
         },
       },
     },
