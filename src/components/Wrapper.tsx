79fd58d6fb58934c53c986777cdd730d54b21d71
João Francisco <joao.rodrigues@viasoft.com.br>
2023-06-29 19:06:43 -0300

    refactor: organizacao wrapper

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -1,15 +1,25 @@
-import { Box } from '@chakra-ui/react';
-import React from 'react'
+import { Box } from "@chakra-ui/react";
+import React from "react";
+
+export type WrapperVariant = "small" | "regular";
 
 interface WrapperProps {
-  children?: any;
-  variant?: 'small' | 'regular'
+  children: React.ReactNode;
+  variant?: WrapperVariant;
 }
 
-export const Wrapper: React.FC<WrapperProps> = ({ children, variant }) => {
-    return (
-      <Box maxW={variant === "regular" ? "800px" : "400px"} w="100%" mt={8} mx="auto">
-        { children }
-      </Box>
-    );
-}
+export const Wrapper: React.FC<WrapperProps> = ({
+  children,
+  variant = "regular",
+}) => {
+  return (
+    <Box
+      maxW={variant === "regular" ? "800px" : "400px"}
+      w="100%"
+      mt={8}
+      mx="auto"
+    >
+      {children}
+    </Box>
+  );
+};
