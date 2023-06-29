014a694e3d1a6fa12ed817b002e01223771afb45
João Francisco <joao.rodrigues@viasoft.com.br>
2023-06-29 19:13:55 -0300

    refactor: melhoria tela de login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,52 +1,72 @@
-import { Box, Button, FormControl, FormErrorMessage, FormLabel, Input } from "@chakra-ui/react";
+import { Box, Button, Link } from "@chakra-ui/react";
 import { Form, Formik } from "formik";
+import { withUrqlClient } from "next-urql";
 import { useRouter } from "next/router";
-import React from "react"
+import React from "react";
+
 import { InputField } from "../components/InputField";
 import { Wrapper } from "../components/Wrapper";
 import { useLoginMutation } from "../generated/graphql";
+import { createUrqlClient } from "../utils/createUrqlClient";
 import { toErrorMap } from "../utils/toErrorMap";
 
-interface LoginProps {}
-
-const Login: React.FC<LoginProps> = ({}) => {
+const Login: React.FC = () => {
   const router = useRouter();
   const [, login] = useLoginMutation();
+
   return (
     <Wrapper variant="small">
       <Formik
-        initialValues={{ username: "" as string, password: "" as string }}
+        initialValues={{ usernameOrEmail: "", password: "" }}
         onSubmit={async (values, { setErrors }) => {
-          const response = await login(values);
+          const response = await login({
+            usernameOrEmail: values.usernameOrEmail,
+            password: values.password,
+          });
 
-          if (response.data?.login.errors) {
-            setErrors(toErrorMap(response.data.login.errors));
+          if (response.data?.login.errors?.length) {
+            setErrors(toErrorMap(response.data?.login.errors));
           } else if (response.data?.login.user) {
-            router.push("/");
+            const nextRouter = router.query.next;
+
+            if (typeof nextRouter === "string") {
+              router.push(nextRouter, { pathname: nextRouter });
+            } else {
+              router.push("/", { pathname: "/" });
+            }
           }
+
+          return response;
         }}
       >
         {({ isSubmitting }) => (
           <Form>
-            <InputField 
-              name="username" 
-              placeholder="username" 
-              label="Username" 
+            <InputField
+              name="usernameOrEmail"
+              placeholder="Your username or email goes here..."
+              label="Username/email"
             />
             <Box mt={4}>
-              <InputField 
-                name="password" 
-                placeholder="password"
-                label="Password" 
-                type="password" 
+              <InputField
+                name="password"
+                placeholder="And your secret password here"
+                label="Password"
+                type="password"
               />
             </Box>
-            <Button mt={4} type="submit" colorScheme="teal" isLoading={isSubmitting}> login</Button>
+
+            <Box mt={2} color="primary">
+              <Link href="/forgot-password">Oops, forgot your password?</Link>
+            </Box>
+
+            <Button mt={4} color="teal" type="submit" isLoading={isSubmitting}>
+              Login
+            </Button>
           </Form>
         )}
       </Formik>
     </Wrapper>
-  ); 
-}
+  );
+};
 
-export default Login;
+export default withUrqlClient(createUrqlClient)(Login);
