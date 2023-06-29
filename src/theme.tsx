281ab73da9579ac59c8f32383ad695eddece81a2
João Francisco <joao.rodrigues@viasoft.com.br>
2023-06-29 19:03:30 -0300

    feat: create breakpoints

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,13 +1,14 @@
 import { extendTheme } from '@chakra-ui/react'
+import { createBreakpoints } from '@chakra-ui/theme-tools'
 
 const fonts = { mono: `'Menlo', monospace` }
 
-const breakpoints = {
+const breakpoints = createBreakpoints({
   sm: '40em',
   md: '52em',
   lg: '64em',
   xl: '80em',
-}
+})
 
 const theme = extendTheme({
   semanticTokens: {
