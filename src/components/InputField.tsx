79bc84d6cac012818f4386d8659b40b73abfb45e
João Francisco <joao.rodrigues@viasoft.com.br>
2023-06-29 19:05:15 -0300

    feat: melhoria no input field

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,24 +1,44 @@
-import { FormControl, FormErrorMessage, FormLabel, Input } from '@chakra-ui/react';
-import { useField } from 'formik';
-import React, { InputHTMLAttributes } from 'react'
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  FormErrorMessage,
+  Textarea,
+} from "@chakra-ui/react";
+import { useField } from "formik";
+import React, { InputHTMLAttributes } from "react";
 
 type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
-  label: string;
   name: string;
+  label: string;
+  placeholder: string;
+  textarea?: boolean;
 };
 
 // underscore = not used props
-export const InputField: React.FC<InputFieldProps> = ({label, size: _, ...props}) => {
-  const [field, {error}] = useField(props);
-    return (
-      <FormControl isInvalid={!!error}>
-        <FormLabel htmlFor={field.name}>{label}</FormLabel>
-          <Input
-            {...field}
-            {...props}
-            id={field.name}
-          />
-        {error ? <FormErrorMessage>{error}</FormErrorMessage> : null}
-      </FormControl>
-    );
-}
+export const InputField: React.FC<InputFieldProps> = ({
+  label,
+  textarea = false,
+  size: _,
+  ...props
+}) => {
+  let InputOrTextArea = Input as any;
+
+  if (textarea) {
+    InputOrTextArea = Textarea as any;
+  }
+
+  const [field, { error }] = useField(props);
+  return (
+    <FormControl isInvalid={!!error}>
+      <FormLabel>{label}</FormLabel>
+      <InputOrTextArea
+        {...props}
+        {...field}
+        id={field.name}
+        placeholder={props.placeholder}
+      />
+      {error ? <FormErrorMessage>{error}</FormErrorMessage> : null}
+    </FormControl>
+  );
+};
